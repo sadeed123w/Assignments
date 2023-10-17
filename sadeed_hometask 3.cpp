56ec43a8f679df7dbd93f0131f7afbf43573d59c
sadeed123w <147578821+sadeed123w@users.noreply.github.com>
2023-10-17 07:52:31 +0500

    Add files via upload

---
+++ sadeed_hometask 3.cpp
@@ -0,0 +1,175 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	
+	
+	cout<<"TASK NO 1"<<endl;
+	char inp;
+	cout<<"input alphabet"<<endl;
+	cin>>inp;
+	switch(inp){
+		case 'a':
+		cout<<"vovel";
+		break;
+		case 'e':
+		cout<<"vowel";
+		break;
+		case 'i':
+		cout<<"vowel";
+		break;
+		case 'o':
+		cout<<"vowel";
+		break;
+		case 'u':
+		cout<<"vowel";
+		break;
+		case 'A':
+		cout<<"vowel";
+		break;
+		case 'E':
+		cout<<"vowel";
+		break;
+		case 'I':
+		cout<<"vowel";
+		break;
+		case 'O':
+		cout<<"vowel";
+		break;
+		case 'U':
+		cout<<"vowel";
+		break;
+		default:
+		cout<<"consonant";
+		break;
+	}
+
+
+
+
+
+
+
+
+//{
+//	cout<<"TASK 2"<<endl;
+//  int inp;
+//  cout<<"press 1 for punjab "<<endl;
+//  cout<<"press 2 for sindh "<<endl;
+//  cout<<"press 3 for KPK "<<endl;
+//  cout<<"press 4 for Balochistan "<<endl;
+//  cin>>inp;
+//  switch(inp){
+//  case 1:cout<<"population of punjab is"<<endl;
+//  break;
+//  case 2:cout<<"population of sindh is"<<endl;
+//  break;
+//  case 3:cout<<"population of KPK is"<<endl;
+//  break;
+//  case 4:cout<<"population of balochistan is"<<endl;
+//  break;
+//}
+
+
+
+
+
+
+
+//   int age;
+//
+//    cout << "Enter age "<<endl;
+//    cin >> age;
+//
+//    if (age >= 18) {
+//        cout << "The person is an adult" << endl;
+//    }
+//	 else if (age >= 13) {
+//        cout << "The person is a teenager" << endl;
+//    } 
+//	else {
+//        cout << "The person is a child" << endl;
+//    }
+
+
+
+
+
+
+
+
+//    double num;
+//    cout << "Enter a number";
+//    cin >>num;
+//
+//    int r;  
+//
+//    if (num > 0) {
+//        r= 1;  
+//    } else if (num < 0) {
+//        r = -1;  
+//    } else {
+//        r = 0;  
+//    }
+//
+//
+//    switch (r) {
+//        case 1:
+//            cout << "The number is positive" << endl;
+//            break;
+//        case -1:
+//            cout << "The number is negative" << endl;
+//            break;
+//        case 0:
+//            cout << "The number is zero" << endl;
+//            break;
+//        default:
+//            cout << "Invalid input" << endl;
+//    }
+
+
+
+
+
+
+//int a;
+//int b;
+//int c;
+//cout<<"input first num"<<endl;
+//cin>>a;
+//cout<<"input second num"<<endl;
+//cin>>b;
+//cout<<"input third num"<<endl;
+//cin>>c;
+//if(a>b&&a>c){cout<<a;
+//}
+//else {
+//   if(b>a&&b>c){cout<<b;}
+//   else{
+//   if(a=b=c){cout<<"a=b=c"<<endl;}
+//else{cout<<c;}
+//}
+//}
+
+
+
+
+
+
+//char c;
+//char a;
+//char e;
+//char i;
+//char o;
+//char u;
+//cout<<"input alphabet"<<endl;
+//cin>>c;
+//
+//if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){cout<<"alphabet is a vowel"<<endl;
+//}
+//else{cout<<"alphabet is a consonant"<<endl;
+//}
+
+    return 0;
+}
